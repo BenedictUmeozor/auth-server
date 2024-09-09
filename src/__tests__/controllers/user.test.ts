8ce876347bcbecf2242bb909cff412291d0c1cb1
Benedict <benedictumeozor@gmail.com>
2024-09-09 10:56:48 +0100

    feat: get user tests

--- src/__tests__/controllers/user.test.ts
+++ src/__tests__/controllers/user.test.ts
@@ -1,23 +1,135 @@
+import createServer from "../../utils/server";
 import {
+  getUser,
+  getUsers,
   passwordResetRequest,
   resetPassword,
   verifyPasswordRequest,
 } from "../../controllers/user";
 import {
+  getAllUsers,
+  getParticularUser,
   requestPasswordReset,
   resetUserPassword,
   verifyRequest,
 } from "../../services/user";
+import request from "supertest";
+import createHttpError from "http-errors";
 
 jest.mock("../../services/user", () => ({
   requestPasswordReset: jest.fn(),
   resetUserPassword: jest.fn(),
   verifyRequest: jest.fn(),
+  getAllUsers: jest.fn(),
+  getParticularUser: jest.fn(),
 }));
+
+const app = createServer();
+
+const mockUsers = [
+  {
+    _id: "1",
+    email: "ben@example.com",
+    password: "fake-password",
+    name: "Ben",
+  },
+  {
+    _id: "2",
+    email: "john@example.com",
+    password: "fake-password",
+    name: "John",
+  },
+];
+
+const mockUser = {
+  _id: "1",
+  email: "ben@example.com",
+  password: "fake-password",
+  name: "Ben",
+};
 
 describe("User Controllers", () => {
   afterEach(() => {
     jest.clearAllMocks();
+  });
+
+  describe("GetUsers", () => {
+    it("should get users successfully", async () => {
+      (getAllUsers as jest.Mock).mockResolvedValue(mockUsers);
+
+      const response = await request(app).get("/api/users");
+
+      expect(response.status).toBe(200);
+      expect(getAllUsers).toHaveBeenCalled();
+      expect(response.body).toEqual({
+        users: mockUsers,
+        success: true,
+        count: mockUsers.length,
+      });
+    });
+
+    it("should call next if error fails", async () => {
+      const error = new Error("Failed to get users");
+      (getAllUsers as jest.Mock).mockRejectedValue(error);
+
+      const req = jest.fn();
+      const res = jest.fn();
+      const next = jest.fn();
+
+      await getUsers(req as any, res as any, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe("GetUser", () => {
+    it("should get user successfully", async () => {
+      const mockId = "1";
+
+      (getParticularUser as jest.Mock).mockResolvedValue(mockUser);
+
+      const response = await request(app).get(`/api/users/${mockId}`);
+
+      expect(response.status).toBe(200);
+      expect(getParticularUser).toHaveBeenCalledWith(mockId);
+      expect(response.body).toEqual({
+        user: mockUser,
+        success: true,
+      });
+    });
+
+    it("should throw error if id is missing", async () => {
+      const req = {
+        params: {
+          id: "",
+        },
+      };
+
+      const res = jest.fn();
+      const next = jest.fn();
+
+      await getUser(req as any, res as any, next);
+
+      expect(next).toHaveBeenCalledWith(createHttpError(500, "id is missing"));
+    });
+
+    it("should call next if user is not found", async () => {
+      const error = new Error("User not found");
+      (getParticularUser as jest.Mock).mockRejectedValue(error);
+
+      const req = {
+        params: {
+          id: "1",
+        },
+      };
+
+      const res = jest.fn();
+      const next = jest.fn();
+
+      await getUser(req as any, res as any, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
   });
 
   describe("PasswordResetRequest", () => {
--- src/__tests__/services/user.test.ts
+++ src/__tests__/services/user.test.ts
@@ -36,107 +36,109 @@ const mockUser = {
   password: mockPassword,
 };
 
-describe("requestPasswordReset", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("should send mail successfully", async () => {
-    mockVariable(User.exists).mockResolvedValue(true);
-    mockVariable(OTP.deleteMany).mockResolvedValue(true);
-    mockVariable(generateOTP).mockReturnValue(mockOTP);
-    mockVariable(OTP.create).mockResolvedValue(mockOTP);
-    mockVariable(sendMail).mockResolvedValue(undefined);
-
-    const result = await requestPasswordReset(mockEmail);
-
-    expect(User.exists).toHaveBeenCalledWith({ email: mockEmail });
-    expect(OTP.deleteMany).toHaveBeenCalledWith({ email: mockEmail });
-    expect(sendMail).toHaveBeenCalled();
-    expect(result).toEqual({ message: "OTP sent successfully" });
-  });
-
-  it("should throw an error when user does not exist", async () => {
-    mockVariable(User.exists).mockResolvedValue(false);
-
-    try {
-      await requestPasswordReset(mockEmail);
-      fail("Function did not throw an error");
-    } catch (error) {
-      expect(error).toEqual(createHttpError(404, "User not found"));
-    }
-  });
-});
-
-describe("verifyRequest", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("should verify request successfully", async () => {
-    mockVariable(User.exists).mockResolvedValue(true);
-    mockVariable(OTP.findOne).mockResolvedValue(mockOTP);
-    mockVariable(validateOTP).mockReturnValue(true);
-
-    const result = await verifyRequest(mockEmail, mockOTP.otp);
-
-    expect(User.exists).toHaveBeenCalledWith({ email: mockEmail });
-    expect(OTP.findOne).toHaveBeenCalledWith({
-      email: mockEmail,
-      otp: mockOTP.otp,
+describe("User Service", () => {
+  describe("requestPasswordReset", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
     });
-    expect(OTP.deleteMany).toHaveBeenCalledWith({ email: mockEmail });
-    expect(result).toEqual({ message: "OTP verified successfully" });
-  });
 
-  it("should not run if email does not exist", async () => {
-    mockVariable(User.exists).mockResolvedValue(false);
+    it("should send mail successfully", async () => {
+      mockVariable(User.exists).mockResolvedValue(true);
+      mockVariable(OTP.deleteMany).mockResolvedValue(true);
+      mockVariable(generateOTP).mockReturnValue(mockOTP);
+      mockVariable(OTP.create).mockResolvedValue(mockOTP);
+      mockVariable(sendMail).mockResolvedValue(undefined);
 
-    try {
-      await verifyRequest(mockEmail, mockOTP.otp);
-      fail("Function did not throw an error");
-    } catch (error) {
-      expect(error).toEqual(createHttpError(404, "User not found"));
-    }
-  });
+      const result = await requestPasswordReset(mockEmail);
 
-  it("should not run if otp is not found", async () => {
-    mockVariable(User.exists).mockResolvedValue(true);
-    mockVariable(OTP.findOne).mockResolvedValue(null);
-
-    try {
-      await verifyRequest(mockEmail, mockOTP.otp);
-      fail("Function did not throw an error");
-    } catch (error) {
-      expect(error).toEqual(createHttpError(404, "Invalid OTP"));
-    }
-  });
-});
-
-describe("resetUserPassword", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("should reset password successfully", async () => {
-    mockVariable(User.findOneAndUpdate).mockResolvedValue({
-      email: mockEmail,
+      expect(User.exists).toHaveBeenCalledWith({ email: mockEmail });
+      expect(OTP.deleteMany).toHaveBeenCalledWith({ email: mockEmail });
+      expect(sendMail).toHaveBeenCalled();
+      expect(result).toEqual({ message: "OTP sent successfully" });
     });
-    mockVariable(bcrypt.hash).mockResolvedValue(mockPassword);
-    const result = await resetUserPassword(mockEmail, mockPassword);
 
-    expect(bcrypt.hash).toHaveBeenCalledWith(mockPassword, 10);
-    expect(result).toEqual({ message: "Password reset successfully" });
+    it("should throw an error when user does not exist", async () => {
+      mockVariable(User.exists).mockResolvedValue(false);
+
+      try {
+        await requestPasswordReset(mockEmail);
+        fail("Function did not throw an error");
+      } catch (error) {
+        expect(error).toEqual(createHttpError(404, "User not found"));
+      }
+    });
   });
 
-  it("should throw an error if user is not found", async () => {
-    mockVariable(User.findOneAndUpdate).mockResolvedValue(null);
+  describe("verifyRequest", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
 
-    try {
-      await resetUserPassword(mockEmail, mockPassword);
-      fail("Function did not throw an error");
-    } catch (error) {
-      expect(error).toEqual(createHttpError(404, "User not found"));
-    }
+    it("should verify request successfully", async () => {
+      mockVariable(User.exists).mockResolvedValue(true);
+      mockVariable(OTP.findOne).mockResolvedValue(mockOTP);
+      mockVariable(validateOTP).mockReturnValue(true);
+
+      const result = await verifyRequest(mockEmail, mockOTP.otp);
+
+      expect(User.exists).toHaveBeenCalledWith({ email: mockEmail });
+      expect(OTP.findOne).toHaveBeenCalledWith({
+        email: mockEmail,
+        otp: mockOTP.otp,
+      });
+      expect(OTP.deleteMany).toHaveBeenCalledWith({ email: mockEmail });
+      expect(result).toEqual({ message: "OTP verified successfully" });
+    });
+
+    it("should not run if email does not exist", async () => {
+      mockVariable(User.exists).mockResolvedValue(false);
+
+      try {
+        await verifyRequest(mockEmail, mockOTP.otp);
+        fail("Function did not throw an error");
+      } catch (error) {
+        expect(error).toEqual(createHttpError(404, "User not found"));
+      }
+    });
+
+    it("should not run if otp is not found", async () => {
+      mockVariable(User.exists).mockResolvedValue(true);
+      mockVariable(OTP.findOne).mockResolvedValue(null);
+
+      try {
+        await verifyRequest(mockEmail, mockOTP.otp);
+        fail("Function did not throw an error");
+      } catch (error) {
+        expect(error).toEqual(createHttpError(404, "Invalid OTP"));
+      }
+    });
+  });
+
+  describe("resetUserPassword", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should reset password successfully", async () => {
+      mockVariable(User.findOneAndUpdate).mockResolvedValue({
+        email: mockEmail,
+      });
+      mockVariable(bcrypt.hash).mockResolvedValue(mockPassword);
+      const result = await resetUserPassword(mockEmail, mockPassword);
+
+      expect(bcrypt.hash).toHaveBeenCalledWith(mockPassword, 10);
+      expect(result).toEqual({ message: "Password reset successfully" });
+    });
+
+    it("should throw an error if user is not found", async () => {
+      mockVariable(User.findOneAndUpdate).mockResolvedValue(null);
+
+      try {
+        await resetUserPassword(mockEmail, mockPassword);
+        fail("Function did not throw an error");
+      } catch (error) {
+        expect(error).toEqual(createHttpError(404, "User not found"));
+      }
+    });
   });
 });
